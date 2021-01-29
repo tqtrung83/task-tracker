26b8c1adbf057c769865ee33ad788a0444f6cdde
Trung Trinh <tqtrung83@gmail.com>
2021-01-29 20:00:16 -0330

    Added Single Task Component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Task = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-  ]);
-
+const Task = ({ task }) => {
   return (
-    <>
-      {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
-      ))}
-    </>
+    <div className='task'>
+      <h3>{task.text}</h3>
+    </div>
   );
 };
 
